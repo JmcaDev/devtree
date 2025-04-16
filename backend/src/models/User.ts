51486f37dbdcdc86572e55f7153d2dedaa1424d1
JmcaDev <jmcavzla@gmail.com>
2025-04-16 19:54:19 -0400

    User Model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+//Schemas
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+})
+
+//Models
+const User = mongoose.model("User", userSchema)
+export default User
